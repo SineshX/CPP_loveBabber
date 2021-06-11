046eb45c09aee50411a321026db7a7e7cd6be130
Sinesh <sineshkumarr@gmail.com>
2021-06-11 07:56:57 +0530

    reverse an array of integers

--- Array1Reverse.cpp
+++ Array1Reverse.cpp
@@ -7,15 +7,30 @@
 
 using namespace std;
 
+void printArray(int arr[],int size)
+{	cout<<"Array :  ";
+	for(int i = 0;i<size;i++)
+	{
+		cout<<arr[i]<<"\t";
+	}
+	cout<<endl;
+}
 int main(void)
 {	
 
-	char arr[] = "RadheShaym";
-	// cout<<"Please Enter the Array  :  ";
-	// cin>>arr;
-	cout<<arr<<endl;
+	int arr[] = {1,2,3,4,5,6,7,8,9};
+
 	//getting size of array
-	int len  = sizeof(arr)/sizeof(arr[0]);
-	cout<<len<<endl;
+	int n  = sizeof(arr)/sizeof(arr[0]);
+	// cout<<len<<endl;
+	for (int i = 0, temp = 0; i < n/2; i++)
+	{
+		temp = arr[i];
+		arr[i]= arr[n-(i+1)];
+		arr[n-(i+1)] = temp;
+	}
+
+	printArray(arr,n);
+	
 	return 0;
 }
