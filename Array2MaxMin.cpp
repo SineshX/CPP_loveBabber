a4fb14af086002be64c1050f8554eff72537e0a6
Sinesh <sineshkumarr@gmail.com>
2021-06-11 09:10:04 +0530

    min max using function and structure

--- Array2MaxMin.cpp
+++ Array2MaxMin.cpp
@@ -1,20 +1,20 @@
 /*
  * Find Maximum and minimum element of an Array
+ * using function 
  */
-
 
 #include<iostream>
 
 using namespace std;
 
-void printArray(int arr[],int size)
-{	cout<<"Array :  ";
-	for(int i = 0;i<size;i++)
-	{
-		cout<<arr[i]<<"\t";
-	}
-	cout<<endl;
-}
+struct Pair{
+    int min = 0 ;
+    int max = 0 ;
+};// name of my structure 
+
+void printArray(int arr[],int size);
+Pair minmaxFun(int arr[],int size);
+
 int main(void)
 {	
 
@@ -25,20 +25,53 @@ int main(void)
 
 	printArray(arr,n);
 
-    //finding min or max
-	int min = arr[0],max = arr[0] ;
-	for (int i = 1; i < n; i++)
-	{
-		if(arr[i] < min)
-            min = arr[i];
-        if(arr[i] > max)
-            max = arr[i];
-        
-	}
+	Pair mypair; // Pair is user defined DS
+    mypair = minmaxFun(arr,n);
 
-    cout<<"Minimum  : "<<min
-        <<"\nMaximum  : "<<max<<endl;
+    cout<<"Minimum  : "<<mypair.min
+        <<"\nMaximum  : "<<mypair.max<<endl;
 
 	
 	return 0;
+} // end of main function
+
+Pair minmaxFun(int arr[],int size)
+{
+    Pair mypair;
+
+    //init mypair
+    if(size == 1)
+    {
+        mypair.min = arr[0];
+        mypair.max = arr[0];
+    }
+    else if (arr[0] < arr[1])
+    {
+        mypair.min = arr[0];
+        mypair.max = arr[1];
+    }
+    else{
+        mypair.min = arr[1];
+        mypair.max = arr[0];
+    }
+
+    //finding minmax pair
+    for (int i = 2; i < size; i++)
+    {
+        if(arr[i] < mypair.min)
+            mypair.min = arr[i];
+        else if(arr[i] > mypair.max)
+            mypair.max = arr[i];
+    }
+    
+    return mypair;
+}
+
+void printArray(int arr[],int size)
+{	cout<<"Array :  ";
+	for(int i = 0;i<size;i++)
+	{
+		cout<<arr[i]<<"\t";
+	}
+	cout<<endl;
 }
