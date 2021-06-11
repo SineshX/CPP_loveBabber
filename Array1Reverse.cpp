12efa155c2266dce1712dded313f9166a7ea9953
Sinesh <sineshkumarr@gmail.com>
2021-06-11 07:42:31 +0530

    init

---
+++ Array1Reverse.cpp
@@ -0,0 +1,21 @@
+/*
+ * Reverse the Array (String)
+ */
+
+
+#include<iostream>
+
+using namespace std;
+
+int main(void)
+{	
+
+	char arr[] = "RadheShaym";
+	// cout<<"Please Enter the Array  :  ";
+	// cin>>arr;
+	cout<<arr<<endl;
+	//getting size of array
+	int len  = sizeof(arr)/sizeof(arr[0]);
+	cout<<len<<endl;
+	return 0;
+}
